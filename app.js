dcb50927261591bd6dcf749918d7217f93048db6
Thayse342 <thayse342@gmail.com>
2025-03-02 15:46:50 -0300

    Implementado função atualizar lista de amigos

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 let amigos = [Ana,Bea,Rafael,Paulo,Toni,Suzane,Vithor,Viviane];
+
+
+function atualizarListaAmigos() {
+    let listaAmigos = document.getElementById("listaAmigos");
+    listaAmigos.innerHTML = "";
+
+    for (let i = 0; i < amigos.length; i++) {
+    let novoItem = document.createElement("li");
+    novoItem.textContent = amigos[i];
+    listaAmigos.appendChild(novoItem);
+    }
+}
 atualizarListaAmigos();
