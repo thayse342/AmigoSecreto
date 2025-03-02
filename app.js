da4d82c75615e0e0342a30f5e069f2fc53d51370
Thayse342 <thayse342@gmail.com>
2025-03-02 15:36:38 -0300

    Implementa lista de função para atualizar lista de amigos

--- app.js
+++ app.js
@@ -1,2 +1,3 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 let amigos = [Ana,Bea,Rafael,Paulo,Toni,Suzane,Vithor,Viviane];
+atualizarListaAmigos();
