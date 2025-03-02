e45a934e7d28ef9e9656da94682229a40379c331
Thayse342 <thayse342@gmail.com>
2025-03-02 16:49:53 -0300

    Corrigir declaração de variável amigos

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
-let amigos = [Ana,Bea,Rafael,Paulo,Toni,Suzane,Vithor,Viviane];
+let amigos = ["Ana","Bea","Rafael","Paulo","Toni","Suzane","Vithor","Viviane"];
 
 
 function atualizarListaAmigos() {
